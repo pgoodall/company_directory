006e6dd85a728b0e38f21c9c8ffbe6c73f9daeb3
Pete Goodall <pete@digicratic.com>
2026-01-30 15:37:26 +0000

    Just saving work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn read_cli_args() -> Vec<String> {
+    use std::env;
+    let args: Vec<String> = env::args().collect();
+    return args
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = read_cli_args();
+    for arg in &args {
+        println!("{arg}");
+    }
 }
